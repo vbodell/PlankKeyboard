a1f62eea7d2b7019320017ad2ccc0d19f191adc2
Victor Bodell <vbodell@kth.se>
2019-10-14 14:27:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PlankKeyboard
+
+A custom android keyboard to facilitate transcription 
+(sv: transkribera, slang: planka) of music using 
+Roman Numeral Analysis (sv: Steganalys). 
+
+## Screenshot
+![screenshot of PlankKeyboard](screenshots/s1.jpg "Example of PlankKeyboard usage")
+
---
+++ app/src/main/java/vbodell/dev/androidcustomkeyboard/PlankKeyboard.java
@@ -0,0 +1,149 @@
+package vbodell.dev.androidcustomkeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+
+public class PlankKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private  boolean isCaps = false;
+
+
+    //Press Ctrl+O
+
+
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard,null);
+        keyboard = new Keyboard(this,R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+        InputConnection ic = getCurrentInputConnection();
+        switch (i)
+        {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1,0);
+            break;
+            case Keyboard.KEYCODE_SHIFT:
+                isCaps = !isCaps;
+                keyboard.setShifted(isCaps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_ENTER));
+                break;
+            case 44: // Append ' '
+                ic.commitText(", ", 1);
+                break;
+            case 45: //-
+                ic.commitText("->", 1);
+                break;
+            case 731:
+                ic.commitText("IIm", 1);
+                break;
+            case 732:
+                ic.commitText("IIIm", 1);
+                break;
+            case 860:
+                ic.commitText("IV", 1);
+                break;
+            case 861:
+                ic.commitText("VIm", 1);
+                break;
+            case 862:
+                ic.commitText("VII", 1);
+                break;
+            case 996:
+                ic.commitText("°", 1);
+                break;
+            case 997:
+                ic.commitText("ø", 1);
+                break;
+            case 998:
+                ic.commitText("∆", 1);
+                break;
+            case 999:
+                InputMethodManager imeManager = (InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+                imeManager.showInputMethodPicker();
+                break;
+                default:
+                    char code = (char)i;
+                    if(Character.isLetter(code) && isCaps)
+                        code = Character.toUpperCase(code);
+                    ic.commitText(String.valueOf(code),1);
+        }
+
+    }
+
+    private void playClick(int i) {
+
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch(i)
+        {
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
